7e1627e424dcef8171d4f6b9e5156fcc931bfb58
Pushkar291191 <pushkar291191@gmail.com>
2017-02-15 06:25:47 +0000

    Fixed the issue with Contacts Controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,15 +1,21 @@
 class ContactsController < ApplicationController
   def new
-    @contact = Contact.new
+  @contact = Contact.new
   end
   
   def create
-    @contact = Contact.new(Contact_params)
+    @contact = Contact.new(contact_params)
     if @contact.save
-      redirect_to new_contact_path, notice: "Message sent."
+      redirect_to new_contact_path, notice: "Message Sent"
     else
-      redirect_to new_contact_path, notice: "Error occured."
+      redirect_to new_contact_path, notice: "Message not Sent"
     end
-  end
+  end    
+  
+  
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
   
 end
