56c779bc96831a1b655ad357cef8415d073852be
Pushkar291191 <pushkar291191@gmail.com>
2017-02-15 06:15:05 +0000

    Broken ContactsController

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,15 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
   end
+  
+  def create
+    @contact = Contact.new(Contact_params)
+    if @contact.save
+      redirect_to new_contact_path, notice: "Message sent."
+    else
+      redirect_to new_contact_path, notice: "Error occured."
+    end
+  end
+  
 end
